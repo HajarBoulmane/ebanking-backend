2887ec3109895a995221fe9c4c3d1d2e286c03fd
HajarBoulmane <hajarboulmane2003@gmail.com>
2026-05-08 18:36:56 +0100

    MySql integration

--- src/main/java/net/hajar/ebankingbackend/entities/BankAccount.java
+++ src/main/java/net/hajar/ebankingbackend/entities/BankAccount.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor @AllArgsConstructor
 
-public class BankAccount {
+public abstract class BankAccount {
     @Id
     private String id;
     private double balance;
@@ -24,6 +24,6 @@ public class BankAccount {
     private AccountStatus status;
     @ManyToOne
     private Customer customer;
-    @OneToMany(mappedBy = "bankAccount")
+    @OneToMany(mappedBy = "bankAccount",fetch = FetchType.LAZY)
     private List<AccountOperation> accountOperations;
 }
